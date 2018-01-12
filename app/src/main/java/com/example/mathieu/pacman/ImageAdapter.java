5da2e3bf38533d5b6439a6d30d07e7a1768ffa19
Mathieu33260 <mathieu.dumez@etu.u-bordeaux.fr>
2018-01-12 12:11:29 +0100

    init

---
+++ app/src/main/java/com/example/mathieu/pacman/ImageAdapter.java
@@ -0,0 +1,92 @@
+package com.example.mathieu.pacman;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
+
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    private ArrayList<Integer> tabGrid = new ArrayList<>();
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+
+        InputStream inputStream = mContext.getResources().openRawResource(R.raw.grid);
+        String stringFile = readTextFile(inputStream);
+        String str[] = stringFile.split(",");
+        for(String s:str) {
+            System.out.println(s);
+        }
+
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(50, 50));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[1]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.blue, R.drawable.circle, R.drawable.white
+    };
+
+    public String readTextFile(InputStream inputStream) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        byte buf[] = new byte[1024];
+        int len;
+        try {
+            while ((len = inputStream.read(buf)) != -1) {
+                outputStream.write(buf, 0, len);
+            }
+            outputStream.close();
+            inputStream.close();
+        } catch (IOException e) {
+
+        }
+        return outputStream.toString();
+    }
+
+}
